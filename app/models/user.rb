89e03870d836190a5dcf292e624efbdfd534de4c
杉原 <shinta_16n1200325@nnn.ed.jp>
2019-05-29 13:13:45 +0900

    nameとemailのvalidationを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase } # 引数に email.downcase! と入れても可
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
